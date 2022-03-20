40d9eee3e5b407b029cafcbf48c3a4d563b5bdbd
Fernando de Paula <dipaula018@gmail.com>
2022-03-19 21:13:21 -0300

    test: tests ok

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import List from "./components/List";
+
+function App() {
+  return <List initialItems={["Fernando", "Rodrigo", "Thiago"]} />;
+}
+
+export default App;
